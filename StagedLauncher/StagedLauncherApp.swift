2dff0630b43f21debf1b2595b8b8d0eff250e270
Tao Xu <360470+hewigovens@users.noreply.github.com>
2025-04-15 17:21:31 +0900

    Initial Commit

---
+++ StagedLauncher/StagedLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  StagedLauncherApp.swift
+//  StagedLauncher
+//
+//  Created by Tao Xu on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct StagedLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
